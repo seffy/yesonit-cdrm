f123d5d3ab1be9bb80e97f99e88c5518ad6548b9
Joseph Jhon Sabana <seffy.sabana@gmail.com>
2025-05-14 02:55:21 +1000

    update yaml for GKE

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ if (!fs.existsSync(uploadsDir)) {
 
 
 // Start server
-const PORT = process.env.PORT || 3738;
+const PORT = process.env.PORT || 8087;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
